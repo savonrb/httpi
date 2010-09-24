c7628d47654592ab17e33cd4465467405c840bde
rubiii <me@rubiii.com>
2010-09-24 23:04:46 +0200

    added integration specs for HTTP GET and POST requests

--- spec/integration/auth_spec.rb
+++ spec/integration/auth_spec.rb
@@ -10,10 +10,21 @@ describe HTTPI do
   before :all do
     @username = @password = "user1"
     @error_message = "Authorization Required"
+    @not_found = "Not Found"
   end
 
   HTTPI::Adapter.adapters.keys.each do |adapter|
     context "using :#{adapter}" do
+      it "should execute an HTTP GET request" do
+        response = HTTPI.get "http://test.webdav.org/dav/"
+        response.body.should include(@not_found)
+      end
+
+      it "should execute an HTTP POST request" do
+        response = HTTPI.post "http://test.webdav.org/dav/"
+        response.body.should include(@not_found)
+      end
+
       context "with HTTP basic authentication" do
         it "requires a username and password" do
           request = HTTPI::Request.new :url => "http://test.webdav.org/auth-basic/"
