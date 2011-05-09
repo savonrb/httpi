687d6001ca865230d11a0eecfd0c66fc24c873d8
rubiii <me@rubiii.com>
2011-05-09 23:21:39 +0200

    added a spec forthe last commit

--- spec/httpi/adapter_spec.rb
+++ spec/httpi/adapter_spec.rb
@@ -6,17 +6,26 @@ describe HTTPI::Adapter do
   let(:adapter) { HTTPI::Adapter }
 
   describe ".use" do
+    around do |example|
+      adapter.use = nil
+      example.run
+      adapter.use = nil
+    end
+
     it "should set the adapter to use" do
       adapter.use.should_not == :curb
 
       adapter.use = :curb
       adapter.use.should == :curb
-
-      adapter.use = nil  # reset
     end
 
     it "should default to use the HTTPClient adapter" do
       adapter.use.should == :httpclient
+    end
+
+    it "should load the adapter's client library" do
+      adapter.expects(:require).with("httpclient")
+      adapter.use = :httpclient
     end
 
     it "should raise an ArgumentError in case of an invalid adapter" do
