00eb708be095dd224928b3765674bdff47ad5a6f
Peter Cai <222655+pcai@users.noreply.github.com>
2026-09-14 14:16:51 -0400

    Parenthesize ternary condition (standardrb)

--- spec/integration/support/digest_auth.rb
+++ spec/integration/support/digest_auth.rb
@@ -95,7 +95,7 @@ class IntegrationServer
 
       def to_s
         map do |k, v|
-          "#{k}=#{(UNQUOTED.include?(k) ? v.to_s : quote(v))}"
+          "#{k}=#{(UNQUOTED.include?(k)) ? v.to_s : quote(v)}"
         end.join(", ")
       end
 
